67612eeb6268c00d2ba71c6a362de38f7c3d15fd
Thomas Sickert <thomas.sickert@gmail.com>
2020-08-11 19:25:49 -1000

    Updated curl command to work correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-curl -v --location --request POST $1 \
---header 'Content-Type: application/json' \
---header 'Cookie: __cfduid=d6fe830db915d18fc740fd4eaf00e01081597171427; __cfruid=9ab0050629c3deec47f5134ab6adaf6620b16d72-1597173154' \
---data-raw '{
-    "content": ""${2}"",
-    "username": ""${3}"",
-    "avatar_url": ""${4}""
-}'
+content=$2
+username=$3
+avatar_url=$4
+
+generate_post_data()
+{
+  cat <<EOF
+{
+    "content": "$content",
+    "username": "$username",
+    "avatar_url": "$avatar_url"
+}
+EOF
+}
+
+curl --location --request POST $1 \
+    --header 'Content-Type: application/json' \
+    --data-raw "$(generate_post_data)"
+
