e0b020e9237130b5d015610f03e8d215e83febf8
tsickert <thomas.sickert@gmail.com>
2024-03-30 17:23:19 +0000

    Fix tests

--- __tests__/webhook.test.ts
+++ __tests__/webhook.test.ts
@@ -1,4 +1,5 @@
 import {executeWebhook} from '../src/webhook'
+import {expect, test} from '@jest/globals'
 
 test('fails with missing URL', async () => {
   await expect(executeWebhook()).rejects.toThrow('Invalid URL')
